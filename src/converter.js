7ed942266f971bd3cc6a4fe62fad166174bdb85f
Trish Fox-Collis <tfoxcollis@gmail.com>
2023-02-05 20:45:52 -0800

    createCustomers method to remove headers, split by commas, and create customer obj, add each customer to converters customers

--- src/converter.js
+++ src/converter.js
@@ -1,14 +1,37 @@
 const fs = require("fs")
+const Customer = require("./customer")
 
 class Converter {
   constructor () {
-
+    this.customers = []
   }
 
   readCsv(filePath) {
     let csv = fs.readFileSync(filePath)
     let csvArr = csv.toString().split("\r")
     return csvArr
+  }
+
+  createCustomers(custArr) {
+    //remove headers
+    custArr.shift()
+    custArr.forEach((customer) => {
+      const splitCust = customer.split(',')
+      let custObj = {
+        CustomerID: splitCust[0],
+        FirstName: splitCust[1],
+        Lastname: splitCust[2],
+        Address01: splitCust[3],
+        Address02: splitCust[4],
+        City: splitCust[5],
+        State: splitCust[6],
+        ZipCode: splitCust[7]
+      }
+
+      this.customers.push(new Customer(custObj))
+    })
+    return 'Success!'
+    
   }
 }
 
--- test/converter-test.js
+++ test/converter-test.js
@@ -4,23 +4,32 @@ var Converter = require('../src/converter.js')
 describe('Converter', function() {
 
   it('should be an instance of converter', function() {
-    var converter = new Converter();
+    let converter = new Converter();
 
     assert.instanceOf(converter, Converter)
   })
   it('should take in a csv file and read it', function() {
-    var converter = new Converter()
-    var testData = 'CustomerID,FirstName,Lastname,Address01,Address02,City,State,ZipCode'
-    var response1 = converter.readCsv('./data/Group01.csv')
+    let converter = new Converter()
+    let testData = 'CustomerID,FirstName,Lastname,Address01,Address02,City,State,ZipCode'
+    let response1 = converter.readCsv('./data/Group01.csv')
 
     assert.equal(response1[0], testData)
 
-    var response2 = converter.readCsv('./data/Group02.csv')
-    var response3 = converter.readCsv('./data/Group03.csv')
+    let response2 = converter.readCsv('./data/Group02.csv')
+    let response3 = converter.readCsv('./data/Group03.csv')
 
     assert.equal(response2[0], testData)
     assert.equal(response3[0], testData)
 
+  })
+  it('should create customers', function() {
+    let converter = new Converter()
+    let response1 = converter.readCsv('./data/Group01.csv')
+
+    let success = converter.createCustomers(response1)
+    assert.equal(success, "Success!")
+    assert.equal(converter.customers.length, 9736)
+    assert.equal(converter.customers[0].ZipCode, 19348)
   })
 
 })
